67ae6d76339631114d8c8c8365a03d1ea113b1b9
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-14 12:56:05 +0600

    home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Intro from "../Component/HomeComponent/Intro/Intro";
+import Ads from "../Component/HomeComponent/Ads/Ads";
+
+const Home = () => {
+  return (
+    <>
+      <Intro />
+      <Ads />
+    </>
+  );
+};
+
+export default Home;
